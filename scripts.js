c4fc1cd6fc28ac6d780ef8227caa194cf4aa6130
HaianCao <caohaian2005@gmail.com>
2023-12-05 11:13:47 +0700

    fixbug navigation bar

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function menu(){
-    var x = document.getElementById("dropdown");
-    if(x.style.display==='none'){
-        x.style.display='block';
-    }
-    else{
-        x.style.display='none';
-    }
-}
